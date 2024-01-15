84745d67c65f3a96e762f4890b1acd879c0a8335
Alisha <137110029+itsAlisha@users.noreply.github.com>
2024-01-15 22:01:20 +0530

    Add files via upload

---
+++ Project 1 - myreactapp/src/pictures/Fdata.jsx
@@ -0,0 +1,34 @@
+const Fdata = [
+  {
+    id: 1,
+    fname: "Daal Makhni",
+    imgsrc:
+      "https://www.funfoodfrolic.com/wp-content/uploads/2023/04/Dal-Makhani-Blog.jpg",
+    recipe:
+      "https://www.maggi.in/en/recipes/dal-makhani/?gad_source=1&gclid=CjwKCAiA-bmsBhAGEiwAoaQNmimcvq56vNe_qLV48bovhSux5wGY1zy5fLc1sxT6KKK9pWtLz2-2GxoCvhwQAvD_BwE&gclsrc=aw.ds&utm_source=Google&utm_medium=cpc&utm_campaign=IN_MAGG_mgg_en_GO_NonBrand_EX_NA_CON_FLWDRS_NA_Own_NA",
+  },
+  {
+    id: 2,
+    fname: "Naan",
+    imgsrc:
+      "https://www.vegrecipesofindia.com/wp-content/uploads/2022/12/garlic-naan-3.jpg",
+    recipe: "https://www.indianhealthyrecipes.com/naan/",
+  },
+  {
+    id: 3,
+    fname: "Pizza",
+    imgsrc:
+      "https://www.superhealthykids.com/wp-content/uploads/2021/10/best-veggie-pizza-featured-image-square-2-500x500.jpg",
+    recipe: "https://www.indianhealthyrecipes.com/pizza-recipe-make-pizza/",
+  },
+  {
+    id: 4,
+    fname: "Manchurian",
+    imgsrc:
+      "https://c.ndtvimg.com/2019-11/s5i8nm3g_manchurian_625x300_26_November_19.jpg",
+    recipe:
+      "https://www.vegrecipesofindia.com/veg-manchurian-veg-manchurian-gravy/",
+  },
+];
+
+export default Fdata;
